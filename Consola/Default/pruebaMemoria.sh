7ab9f9f1e477f41bd86dce0c02bb6c568736865c
lucasDeferrari <ldeferrari@frba.utn.edu.ar>
2023-07-09 04:13:22 +0000

    agrego pruebas memoria

---
+++ Consola/Default/pruebaMemoria.sh
@@ -0,0 +1,3 @@
+./Consola "../consola.config" "/home/utnso/tp-2023-1c-Los-operadores/Consola/pruebaMemoria/memoria_1.txt" 
+./Consola "../consola.config" "/home/utnso/tp-2023-1c-Los-operadores/Consola/pruebaMemoria/memoria_2.txt" 
+./Consola "../consola.config" "/home/utnso/tp-2023-1c-Los-operadores/Consola/pruebaMemoria/memoria_3.txt" & wait
