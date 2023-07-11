0408a61c46dcf4b755f150a28f3c645cc063ae0b
ZoeGolfieri <gzoe@frba.utn.edu.ar>
2023-07-11 06:44:52 +0000

    Termino casi todas las pruebas

---
+++ Consola/Default/pruebaFS.sh
@@ -0,0 +1,4 @@
+./Consola "../consola.config" "/home/utnso/tp-2023-1c-Los-operadores/Consola/pruebaFileSystem/FS1.txt" 
+./Consola "../consola.config" "/home/utnso/tp-2023-1c-Los-operadores/Consola/pruebaFileSystem/FS2.txt" 
+
+
---
+++ Consola/Default/pruebaFSextra.sh
@@ -0,0 +1,5 @@
+./Consola "../consola.config" "/home/utnso/tp-2023-1c-Los-operadores/Consola/pruebaFileSystem/FS3.txt" 
+./Consola "../consola.config" "/home/utnso/tp-2023-1c-Los-operadores/Consola/pruebaFileSystem/FS3.txt" 
+./Consola "../consola.config" "/home/utnso/tp-2023-1c-Los-operadores/Consola/pruebaFileSystem/FS3.txt" 
+./Consola "../consola.config" "/home/utnso/tp-2023-1c-Los-operadores/Consola/pruebaFileSystem/FS3.txt" 
+
